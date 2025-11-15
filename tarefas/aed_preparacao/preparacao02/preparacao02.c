d1008f87a894ecfdfbd7a0747d44067fef147d25
samuel <samuelteodoro058@gmail.com>
2025-11-15 15:37:14 -0300

    2 últimos exercícios da preparação para a segunda prova

--- tarefas/aed_preparacao/preparacao02/preparacao02.c
+++ tarefas/aed_preparacao/preparacao02/preparacao02.c
@@ -55,16 +55,20 @@ void method_02(){
     for (int i = 0; i<meuArranjo.tamanho; i++){
         fscanf(arquivo, "%lf", &meuArranjo.dados[i]);
     }
-    
 
+    fclose (arquivo); 
+    
+    if (meuArranjo.tamanho > 0){
     double maiorDistancia = acharMaior(meuArranjo);
     double menorDistancia = acharMenor(meuArranjo);
 
     printf("\nMaior distancia: %lf\n", maiorDistancia);
     printf("Menor distancia: %lf\n", menorDistancia);
+    }else{
+        printf("\nERRO: Arquivo SAIDA_1.TXT esta vazio.\n");
+    }
 
     free(meuArranjo.dados);
-    fclose (arquivo); 
 }
 double acharMaior(struct ArranjoDouble meuArranjo){
     double maior = meuArranjo.dados[0];
@@ -125,6 +129,137 @@ void Method_03 (){
 
     free(meuArranjo.dados);
 }
+
+struct matrizDupla {
+    int linhas;
+    int colunas;
+    double **dados;
+};
+
+void Method_04 (){
+    IO_id("Method 04");
+    FILE *arquivo = fopen("DADOS_1.TXT", "r");
+    int contador = 0;
+    double temp, temp2;
+
+    if (arquivo == NULL) {
+        printf("ERRO: Nao foi possivel abrir DADOS_1.TXT\n");
+        return; 
+    }
+
+   while (fscanf(arquivo, "%lf %lf", &temp, &temp2) == 2 && (temp != 0.0 || temp2 != 0.0)){
+        contador++;
+    }
+    fclose(arquivo);
+    printf("Arquivo lido. Encontrados %d pares de valores.\n", contador);
+
+    struct matrizDupla minhaMatriz;
+
+    minhaMatriz.linhas = contador;
+    minhaMatriz.colunas = 2;
+    minhaMatriz.dados = (double **) malloc(minhaMatriz.linhas * sizeof(double *));
+
+    for (int i = 0; i < minhaMatriz.linhas; i++){
+        minhaMatriz.dados[i] = (double *) malloc (minhaMatriz.colunas * sizeof(double ));
+    }
+
+    arquivo = fopen("DADOS_1.TXT", "r");
+     for (int i = 0; i < minhaMatriz.linhas; i++){
+        fscanf(arquivo, "%lf %lf", &minhaMatriz.dados[i][0], &minhaMatriz.dados[i][1]);
+     } 
+     fclose(arquivo);
+     for (int j = 0; j < minhaMatriz.linhas; j++){
+     for (int i = 0; i<minhaMatriz.linhas - 1; i++){
+
+        if ((minhaMatriz.dados[i][0] > minhaMatriz.dados[i+1][0]) || 
+            (minhaMatriz.dados[i][0] == minhaMatriz.dados[i+1][0] && 
+            minhaMatriz.dados[i][1] > minhaMatriz.dados[i+1][1])){
+
+            double *temp;
+
+            temp = minhaMatriz.dados[i];
+            minhaMatriz.dados[i] = minhaMatriz.dados[i+1];
+            minhaMatriz.dados[i+1] = temp;
+        }
+    }
+    }
+    for (int i = 0; i < minhaMatriz.linhas; i++)
+{
+    free(minhaMatriz.dados[i]);
+}
+    free(minhaMatriz.dados);
+}
+
+double calcularDistanciaPrimeiroUltimo(struct matrizDupla minhaMatriz){
+    double x1 = minhaMatriz.dados[0][0];
+    double y1 = minhaMatriz.dados[0][1];
+
+    double x2 = minhaMatriz.dados[minhaMatriz.linhas - 1][0];
+    double y2 = minhaMatriz.dados[minhaMatriz.linhas - 1][1];
+
+    double distancia = sqrt(pow((x2 - x1), 2.0) + pow((y2 - y1), 2.0));
+    return distancia;
+}
+
+void Method_05(){
+    IO_id("Method 04");
+    FILE *arquivo = fopen("DADOS_1.TXT", "r");
+    int contador = 0;
+    double temp, temp2;
+
+    if (arquivo == NULL) {
+        printf("ERRO: Nao foi possivel abrir DADOS_1.TXT\n");
+        return; 
+    }
+
+   while (fscanf(arquivo, "%lf %lf", &temp, &temp2) == 2 && (temp != 0.0 || temp2 != 0.0)){
+        contador++;
+    }
+    fclose(arquivo);
+    printf("Arquivo lido. Encontrados %d pares de valores.\n", contador);
+
+    struct matrizDupla minhaMatriz;
+
+    minhaMatriz.linhas = contador;
+    minhaMatriz.colunas = 2;
+    minhaMatriz.dados = (double **) malloc(minhaMatriz.linhas * sizeof(double *));
+
+    for (int i = 0; i < minhaMatriz.linhas; i++){
+        minhaMatriz.dados[i] = (double *) malloc (minhaMatriz.colunas * sizeof(double ));
+    }
+
+    arquivo = fopen("DADOS_1.TXT", "r");
+     for (int i = 0; i < minhaMatriz.linhas; i++){
+        fscanf(arquivo, "%lf %lf", &minhaMatriz.dados[i][0], &minhaMatriz.dados[i][1]);
+     } 
+     fclose(arquivo);
+     for (int j = 0; j < minhaMatriz.linhas; j++){
+     for (int i = 0; i<minhaMatriz.linhas - 1; i++){
+
+        if ((minhaMatriz.dados[i][0] > minhaMatriz.dados[i+1][0]) || 
+            (minhaMatriz.dados[i][0] == minhaMatriz.dados[i+1][0] && 
+            minhaMatriz.dados[i][1] > minhaMatriz.dados[i+1][1])){
+
+            double *temp;
+
+            temp = minhaMatriz.dados[i];
+            minhaMatriz.dados[i] = minhaMatriz.dados[i+1];
+            minhaMatriz.dados[i+1] = temp;
+        }
+    }
+    }
+    if (minhaMatriz.linhas > 0){
+        double dist = calcularDistanciaPrimeiroUltimo(minhaMatriz);
+        printf("\nDistancia entre o primeiro e o ultimo par ordenado:\n");
+        printf("%lf\n", dist);
+    }
+    for (int i = 0; i < minhaMatriz.linhas; i++)
+{
+    free(minhaMatriz.dados[i]);
+}
+    free(minhaMatriz.dados);
+
+}
 /*
 Funcao principal.
 */
@@ -162,8 +297,8 @@ case 0: /* nao fazer nada */ break;
 case 1: method_01 ( ); break;
 case 2: method_02 ( ); break;
 case 3: method_03 ( ); break;
-//case 4: method_04 ( ); break;
-//case 5: method_05 ( ); break;
+case 4: method_04 ( ); break;
+case 5: method_05 ( ); break;
 default: // comportamento padrao
 printf ( "\nERRO: Opcao invalida.\n" );
 break;
